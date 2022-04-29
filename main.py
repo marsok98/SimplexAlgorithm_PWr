ff775b4b133c84ed86d3d6272bba69dcf07f7056
marsok98 <mariuszeagle98@gmail.com>
2022-04-29 20:19:26 +0200

    add first two function

--- main.py
+++ main.py
@@ -37,7 +37,51 @@ class SimplexTable:
 
 
 
+class Simplex:
+    def __init__(self, A,XB,Z):
+        self.A_equations = A
+        self.XB_limits = XB
+        self.Z_target_function = Z
+        self.CB_coefficients_basis_variable = [0]*len(XB)
+        self.CZ_relative_profit = [0] * len(Z)
+        self.pivot_index_column_max = 0
+        self.pivot_index_row_min = 0
 
+
+    def can_be_improved(self):
+        i=0
+        j=0
+        t=0
+        for i in range(len(self.Z_target_function)):
+            for j in range(len(self.CB_coefficients_basis_variable)):
+                t += self.A_equations[j][i] * self.CB_coefficients_basis_variable[j]
+            print(t)
+            self.CZ_relative_profit[i] = self.Z_target_function[i] - t
+            t = 0
+        print(self.CZ_relative_profit)
+        return any(x > 0 for x in self.CZ_relative_profit[:-1])
+        #jesli cokolwiek jest wieksze od 0 to znaczy ze mozna ulepszyc
+
+    def get_pivot_position(self):
+        #obliczanie indeksu dla zmiennej wejsciowej
+        max_value = max(self.CZ_relative_profit)
+        self.pivot_index_column_max = self.CZ_relative_profit.index(max_value)
+        print(self.pivot_index_column_max)
+
+        #obliczanie indeksu dla zmiennej wyjsciowej
+        list_of_out_criteria = []
+        for i in range(len(self.XB_limits)):
+            list_of_out_criteria.append(self.XB_limits[i] / self.A_equations[i][self.pivot_index_column_max])
+
+        print(list_of_out_criteria)
+        min_value = min(list_of_out_criteria)
+        self.pivot_index_row_min = list_of_out_criteria.index(min_value)
+        print(self.pivot_index_row_min)
+        # max_index_column opisuje to co wejdzie do podstawy - ktora kolumna
+        # min_index_row opisuje to co wyjdzie z podstawy  - ktory wiersz
+
+        pivot_element = self.A_equations[self.pivot_index_row_min][self.pivot_index_column_max]
+        print(pivot_element)
 
 
 
@@ -55,15 +99,24 @@ class SimplexTable:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    A = [[1,1,0,1],[2,1,1,0]]
-    B = [8,10]
-    C = [1,1,0,0]
-    D = [0,0]
-    E = [0,0,0,0]
+    A = [[1,1,0,1],[2,1,1,0]]#glowna tabela, jeden wiersz symbolizuje jedno rownanie
+    B = [8,10] #ograniczenia
+    C = [1,1,0,0] ##funkcja celu
+    D = [0,0] # wspolczynniki przy zmiennych bazowych
+    E = [0,0,0,0] #wskazniki pomocnicze
     t = 0
     c = []
     #zdefiniowane tabele do rozwiazania
     print(A[0][2])
+
+
+    S1=Simplex(A,B,C)
+
+    napis = S1.can_be_improved()
+    print(napis)
+    print("################################")
+    S1.get_pivot_position()
+    print("################################")
 
     for i in range(len(C)):
         for j in range(len(D)):
