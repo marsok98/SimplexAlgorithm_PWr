522780af214c7aa15d3817420c775c7c302bc610
marsok98 <mariuszeagle98@gmail.com>
2022-05-19 00:42:19 +0200

    add README with explain algorithm and task to do

--- new_approach.py
+++ new_approach.py
@@ -1,136 +1,134 @@
 from copy import deepcopy
+
+
 class Simplex:
     def __init__(self, A):
-        self.YS = A #Y stare
-        self.YN = deepcopy(self.YS)
-
-        #Y nowe
+        self.Y = A #Y cala tablica
+        self.Y_new = deepcopy(self.Y) #potrzebne do zamian
         self.pivot_index_column = 0
         self.pivot_index_row = 0
         self.pivot_element = 0
+
         self.B_basis_variable_number = [0] * (len(A[0]) - 1)
 
         for i in range (len(self.B_basis_variable_number)):
             self.B_basis_variable_number[i] = A[0].index(A[0][-1]) + i
-
         #numery zmiennych, np x3, x4,x5
-        #zebysmy wiedzieli potem jak interpretowac wynik
+        #zebYmy wiedzieli potem jak interpretowac wY_newik
 
-    #sprawdzenie czy pierwsza kolumna ma wszystkie elementy dodatnie
+
+
+    ####    Krok - 2    ####
     def check_admissibility(self):
-        for i in range(len(self.YS)):
-            if self.YS[i][0] < 0:
+        for i in range(len(self.Y)):
+            if self.Y[i][0] < 0:
                 return False
         return True
 
 
+    ####    Krok - 3    ####
     def do_admissibility(self):
 
-        #znaleźć wiersz, ktory ma ujemna wartosc w ograniczeniach
-        #--done__
-        #znaleźć min w tym wierszu ale bez 0 elementu
-        #spisac index tej kolumny
-        #--done
-
-        #wyliczyc wspolczynniki, ale ominac wyrazy ujemny
-        #wziac minimum z tych wspolczynnikow a nastepnie z tego index
-        #indexy wziete to indeks naszego pivota
-        #--done--
-
-        #TODO
-        #Zmienic nazwy zmiennych
-        #zrealizowac te funkcje razem z tymi self, zeby byla spojnosc
-        #opisac calosc algorytmu tak, aby w kodzie byly zmienne
-        #zeby sie polapac w tym wszystkim
-
+        ##    Krok - 3a)    ##
         previous_value = 9999
-        row_min_value = 0
-        pivot_column_index = 0
-        pivot_row_index = 0
-        for i in range(len(self.YS)):
-            if self.YS[i][0] < 0:
-                current_value = self.YS[i][0]
+        index_min_value_in_0th_column = 0
+        for i in range(len(self.Y)):
+            if self.Y[i][0] < 0:
+                current_value = self.Y[i][0]
                 if current_value < previous_value:
-                    row_min_value = i
+                    index_min_value_in_0th_column = i
                     previous_value = current_value
 
+        ##    Krok - 3b)    ##
         previous_value = 9999
-        for i in range(len(self.YS[row_min_value])):
+        for i in range(len(self.Y[index_min_value_in_0th_column])):
             if i != 0:
-                if self.YS[row_min_value][i] < previous_value:
-                    pivot_column_index = i
-                    previous_value = self.YS[row_min_value][i]
+                if self.Y[index_min_value_in_0th_column][i] < previous_value:
+                    self.pivot_index_column = i
+                    previous_value = self.Y[index_min_value_in_0th_column][i]
 
-
-
+        ##    Krok - 3c)    ##
         list_of_out_criteria = []
-
-        for i in range(len(self.YS)):
-            if self.YS[i][pivot_column_index] > 0 and self.YS[i][0] != 0 and i != 0: #wszystkie zabezpieczenia
-                list_of_out_criteria.append(self.YS[i][0] / self.YS[i][pivot_column_index])
+        for i in range(len(self.Y)):
+            if self.Y[i][self.pivot_index_column] > 0 and self.Y[i][0] != 0 and i != 0:
+                list_of_out_criteria.append(self.Y[i][0] / self.Y[i][self.pivot_index_column])
             else:
                 list_of_out_criteria.append(99999)  # gdyby chcial dzielic przez 0 to wstaw tam duza liczbe zamiast dzielenia
 
         min_value = min(list_of_out_criteria)
-        pivot_row_index = list_of_out_criteria.index(min_value)
-
-        print(pivot_row_index)
-        print(pivot_column_index)
 
 
-        self.pivot_index_column = pivot_column_index
-        self.pivot_index_row = pivot_row_index
-        self.pivot_element = self.YS[pivot_row_index][pivot_column_index]
-
-
-
-    def can_be_improved(self):
-        return any(x < 0 for x in self.YS[0])
-    #jesli cokolwiek jest mniejsze od 0 we wspolczynnikach funkcji celu
-
-
-    def get_pivot_position(self):
-        min_value = min(self.YS[0])
-        self.pivot_index_column = self.YS[0].index(min_value)
-
-        list_of_out_criteria = []
-        for i in range(len(self.YS)):
-            if self.YS[i][self.pivot_index_column] != 0 and self.YS[i][0] != 0 and i != 0:
-                list_of_out_criteria.append(self.YS[i][0] / self.YS[i][self.pivot_index_column])
-            else:
-                list_of_out_criteria.append(99999)  # gdyby chcial dzielic przez 0 to wstaw tam duza liczbe zamiast dzielenia
-
-        #print(list_of_out_criteria)
-        min_value = min(list_of_out_criteria)
+        ##    Krok - 3d)    ##
         self.pivot_index_row = list_of_out_criteria.index(min_value)
-        self.pivot_element = self.YS[self.pivot_index_row][self.pivot_index_column]
+        self.pivot_element = self.Y[self.pivot_index_row][self.pivot_index_column]
 
+        ##    Krok - 3e)    ##
+        self.pivot_step()
+
+
+    ####    Krok - 4    ####
+    def can_be_improved(self):
+        return any(x < 0 for x in self.Y[0])
+
+    ####    Krok - 5    ####
+    def get_pivot_position(self):
+
+        ##    Krok - 5a)    ##
+        min_value = min(self.Y[0])
+        self.pivot_index_column = self.Y[0].index(min_value)
+
+        ##    Krok - 5b)    ##
+        list_of_out_criteria = []
+        for i in range(len(self.Y)):
+            if self.Y[i][self.pivot_index_column] != 0 and self.Y[i][0] != 0 and i != 0:
+                list_of_out_criteria.append(self.Y[i][0] / self.Y[i][self.pivot_index_column])
+            else:
+                list_of_out_criteria.append(99999)
+        min_value = min(list_of_out_criteria)
+
+        ##    Krok - 5c)    ##
+        self.pivot_index_row = list_of_out_criteria.index(min_value)
+        self.pivot_element = self.Y[self.pivot_index_row][self.pivot_index_column]
+
+    ####    Krok - 6   ####
     def pivot_step(self):
-        for k in range(len(self.YS)):
-            for l in range(len(self.YS[0])):
+        for k in range(len(self.Y)):
+            for l in range(len(self.Y[0])):
+                ##    Krok - 6a)    ##
                 if k == self.pivot_index_row and l == self.pivot_index_column:
-                    self.YN[k][l] = 1 / self.pivot_element
-
+                    self.Y_new[k][l] = 1 / self.pivot_element
+                ##    Krok - 6b)    ##
                 elif k == self.pivot_index_row and l != self.pivot_index_column:
-                    self.YN[k][l] = self.YS[k][l]/self.pivot_element
-
+                    self.Y_new[k][l] = self.Y[k][l]/self.pivot_element
+                ##    Krok - 6c)    ##
                 elif k != self.pivot_index_row and l == self.pivot_index_column:
-                    self.YN[k][l] = (-1) * self.YS[k][l]/self.pivot_element
-
+                    self.Y_new[k][l] = (-1) * self.Y[k][l]/self.pivot_element
+                ##    Krok - 6d)    ##
                 else:
-                    a = self.YS[k][l]
-                    b = self.YS[k][self.pivot_index_column]
-                    c = self.YS[self.pivot_index_row][l]
-                    d = self.pivot_element
-                    self.YN[k][l] = (self.YS[k][l] - (self.YS[k][self.pivot_index_column] * self.YS[self.pivot_index_row][l])/self.pivot_element)
+                    self.Y_new[k][l] = (self.Y[k][l] - (self.Y[k][self.pivot_index_column] * self.Y[self.pivot_index_row][l])/self.pivot_element)
 
-        self.YS = deepcopy(self.YN)
+        self.Y = deepcopy(self.Y_new)
 
+    ####    Krok - 8    ####
     def show_optimal_solution(self):
-        print(self.YS)
+        print(self.Y)
 
 
 if __name__ == '__main__':
+    #TODO
+    # przetestowac z odpowiedziami poszczegolne przejscia i kolejne tablice simplexowe
+    # najlepiej z wykladem
+    # dopuszczanie do rozwiazania, jak i potem szukanie rozwiazan
+    # sprawdzic dla wiekszych instancji jak sie zachowuje algorytm
+    # obsluzyc sytuacje wyjatkowe: te wszystkie zbiory puste i inne ( opisane na wykladzie)
+    # stworzyc wizu
+    # parsing danych z wizu
+    # - obslugiwanie minimum/maximum, ograniczenia wiekszosciowe, mniejszosciowe
+    # - odpowiednie mnozenie przez minus
+    # testy wizu
+    # rysowanie wykresow dla 2 zmiennych
+
+
     A = [[0,-2,-1],[5,1,1],[0,-1,1],[21,6,2]]
     A1 = [[7,0.33,-0.33],[1.5,-0.16,0.66],[3.5,0.16,1.33],[3.5,0.16,0.33]]
 
@@ -139,25 +137,28 @@ if __name__ == '__main__':
     A3 = [[0,-2,-5],[24,1,4],[21,3,1],[9,1,1]]
     A4 = [[0,-2,-5],[6,1,1],[3,0,1],[9,1,2]]
 
+    #dla B dziala robienie dopuszczalnego rozwiazania
     B = [[0,-1,-6],[-2,-2,-1],[3,-1,1],[6,1,1]]
 
+    #dla C nie dziala, ale to jest min, plus same wiekszosciowe ograniczenia
+    C = [[0,1,1],[-8,-1,-2],[-6,-2,-1],[-5,-1,-1]]
 
     S1 = Simplex(B)
 
     print(S1.check_admissibility())
+    #S1.do_admissibility()
+    #S1.pivot_step()
+    #print(S1.can_be_improved())
+    #S1.get_pivot_position()
+    #S1.pivot_step()
+    #print(S1.can_be_improved())
+    #print(S1.Y)
+
+
+
+
+    S1.check_admissibility()
+    print(S1.Y)
     S1.do_admissibility()
-    S1.pivot_step()
-    print(S1.can_be_improved())
-    S1.get_pivot_position()
-    S1.pivot_step()
-    print(S1.can_be_improved())
-
-
-    print(S1.YS)
-
-
-   # while S1.can_be_improved():
-    #    S1.get_pivot_position()
-     #   S1.pivot_step()
-    #S1.show_optimal_solution()
+    print(S1.Y)
 
---
+++ venv/README.txt
@@ -0,0 +1,118 @@
+
+Postać gdy ograniczenia są mniejszościowe:
+-------------------------------------------
+Ponizsze ograniczenia wraz z funkcja celu:
+   x1 + x2     <= 5
+  -x1 + x2     <= 0
+  6x1 + 2x2    <= 21
+
+ maxx0 = 2x1 + x2
+
+ Zapisujemy do postaci tablicy simplexowej:
+
+                   x1      x2
+   x0      0      -2      -1
+   x3      5       1       1
+   x4      0      -1       1
+   x5     21       6       2
+
+Lub w formie już skroconej
+ A = [[0,-2,-1],[5,1,1],[0,-1,1],[21,6,2]]
+
+Co wazne funkcja celu jest z wspolczynnikami ujemnymi
+Wpisywana do tablicy simplexowej
+=====================================================
+
+Postać gdy ograniczenia są wiekszosciowe:
+-----------------------------------------
+Tam gdzie jest ograniczenie wiekszosciowe, nalezy wpisac do tablicy simplexowej ze zmienionymi wszystkimi
+znakami za przeciwne.
+
+  2x1 + 1x2     >= 2
+  -x1 +  x2     <= 3
+  1x1 + 1x2     <= 6
+
+maxx0 = 1x1 + 6x2
+
+                   x1      x2
+   x0      0      -1      -6
+   x3     -2      -2      -1
+   x4      3      -1       1
+   x5      6       1       1
+=====================================================
+
+Kroki rozwiazan simplexa (dwufazowa metoda)
+-------------------------------------------
+1. Zapis danych do tablicy simplexowej.
+
+2. Sprawdzenie dopuszczalnosci rozwiązania początkowego ustawienia tablicy Simplex (czy jesteśmy w jednym z wierzchołkow)
+    a) Jeśli w 0th kolumnie pojawi sie jakas wartosc ujemna, znaczy ze rozwiazanie jest niedopuszczalne - GO TO KROK 3
+    b) Jeśli w 0th kolumnie wszystkie wyrazy będą nieujemne ( 0 lub wiecej) znaczy ze rozwiazanie jest dopuszczalne
+        - nasza tablica reprezentuje wierzcholek simplexa
+        - GO TO KROK 4
+
+3. Zrobienie dopuszczalnosci
+    a) Znajdz minimum w 0th kolumnie i zapisz indeks tego wiersza
+    b) Znajdz minimum w wierszu o indeksie wyliczonym w 3a)
+        - zapewnij zeby nie wzial czegos z 0th kolumny (tam sa ograniczenia i bralismy juz stamtad minimum 3a))
+        - weź indeks tej kolumny
+        - ta zmienna ktora stoi nad ta kolumna bedzie wchodzic do bazy
+    c) Wylicz dzielenie wyrazow z 0th kolumny(ograniczenia) przez kolejne wyrazy z kolumny o indeksie wyliczonym w 3b)
+        - zabezpieczyc przed
+            - wyrazy z kolumny z indeksem nie moga byc mniejsze od 0 ani 0
+            - wyrazy z kolumny 0th nie moga byc 0
+            - nie mozemy operowac na wierszu gdzie jest funkcja celu
+        - weź z tego minimum
+            - wez indeks  z wiersza gdzie bylo minimum
+            - Zmienna stojaca przy tym wierszu wychodzi z bazy
+     d) Okresl pivot
+        - indeks z 3b) to indeks kolumny
+        - indeks z 3c) to indeks wiersza
+     e) Wylicz nową tablice Simplex - Skorzystaj z funkcji z KROKU 6
+     f) GO TO Krok 2 - Ponowne sprawdzenie dopuszczalnosci
+
+4. Sprawdzenie optymalnosci
+    a) Jeśli w 0th wierszu (funkcja celu) jest cokolwiek mniejsze od 0 - mozna ulepszyc
+    b) Jesli w 0th wierszu (funkcja celu) są wszystkie wyrazy są dodatnie - nie mozna ulepszyc - GO TO KROK 8
+
+5. Pobranie pozycji pivota
+    a) Weź indeks wyrazu minimum z 0th wiersza (funkcja celu) - to jest indeks kolumny pivota
+        - ta zmienna ktora stoi nad ta kolumna bedzie wchodzic do bazy
+    b) Wylicz dzielenie wyrazow z 0th kolumny(ograniczenia) przez kolejne wyrazy z kolumny o indeksie wyliczonym w 5a))
+        - zabezpieczyc przed
+            - wyrazy z kolumny z indeksem nie moga byc  0
+            - wyrazy z kolumny 0th nie moga byc 0
+            - nie mozemy operowac na wierszu gdzie jest funkcja celu
+        - weź z tego minimum
+            - wez indeks  z wiersza gdzie bylo minimum
+            - Zmienna stojaca przy tym wierszu wychodzi z bazy
+     c) Okresl pivot
+        - indeks z 5a) to indeks kolumny
+        - indeks z 5b) to indeks wiersza
+
+6. Wylicz nowa tablice Simplex - krok ten pokazuje wszystkie operacje, ktore nalezy wykonac
+    a) liczony element jest ma takie same indeksy jak pivot
+        - Warunek: k == pivot_index_row and l == pivot_index_column:
+        - Wzor: Y_new[k][l] = 1 / pivot_element
+    b) liczone elementy są w tym samym wierszu co pivot
+        - Warunek: k == pivot_index_row and l != pivot_index_column:
+        - Wzor: Y_new[k][l] = Y[k][l]/pivot_element
+    c) liczone elementy są w tej samej kolumnie co pivot:
+        - Warunek: k != pivot_index_row and l == pivot_index_column:
+        - Wzor: Y_new[k][l] = (-1) * Y[k][l]/pivot_element
+    d) wszystko inne, czyli w innej kolumnie i w innym wierszu
+        - Warunek: else
+        - Wzor: Y_new[k][l] = (Y[k][l] - (Y[k][pivot_index_column] * Y[pivot_index_row][l])/pivot_element)
+
+    e) - Dodatek - Jak latwiej zlapac regule do liczenia nowej tablicy Simplex?
+
+           |p    q|   =>   |    1/p         q/p     |
+           |r    s|   =>   |    -r/p     s - rq/p   |
+           Zawsze dzielimy przez Pivot
+
+
+7. GO TO Krok 4
+
+
+8. Pokazanie rozwiazania
+
