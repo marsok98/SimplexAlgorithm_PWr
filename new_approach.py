30cc09421ae4e9ef045b12c3013317b932359b34
marsok98 <mariuszeagle98@gmail.com>
2022-05-18 17:51:35 +0200

    two phase method start working

--- new_approach.py
+++ new_approach.py
@@ -30,18 +30,51 @@ class Simplex:
         #--done__
         #znaleźć min w tym wierszu ale bez 0 elementu
         #spisac index tej kolumny
+        #--done
+
         #wyliczyc wspolczynniki, ale ominac wyrazy ujemny
         #wziac minimum z tych wspolczynnikow a nastepnie z tego index
         #indexy wziete to indeks naszego pivota
 
         previous_value = 9999
         row_min_value = 0
+        pivot_column_index = 0
+        pivot_row_index = 0
         for i in range(len(self.YS)):
             if self.YS[i][0] < 0:
                 current_value = self.YS[i][0]
                 if current_value < previous_value:
                     row_min_value = i
                     previous_value = current_value
+
+        previous_value = 9999
+        for i in range(len(self.YS[row_min_value])):
+            if i != 0:
+                if self.YS[row_min_value][i] < previous_value:
+                    pivot_column_index = i
+                    previous_value = self.YS[row_min_value][i]
+
+
+
+        list_of_out_criteria = []
+
+        for i in range(len(self.YS)):
+            if self.YS[i][pivot_column_index] > 0 and self.YS[i][0] != 0 and i != 0: #wszystkie zabezpieczenia
+                list_of_out_criteria.append(self.YS[i][0] / self.YS[i][pivot_column_index])
+            else:
+                list_of_out_criteria.append(99999)  # gdyby chcial dzielic przez 0 to wstaw tam duza liczbe zamiast dzielenia
+
+        # print(list_of_out_criteria)
+        min_value = min(list_of_out_criteria)
+        pivot_row_index = list_of_out_criteria.index(min_value)
+
+        print(pivot_row_index)
+        print(pivot_column_index)
+
+
+        self.pivot_index_column = pivot_column_index
+        self.pivot_index_row = pivot_row_index
+        self.pivot_element = self.YS[pivot_row_index][pivot_column_index]
 
 
 
@@ -112,6 +145,15 @@ if __name__ == '__main__':
 
     print(S1.check_admissibility())
     S1.do_admissibility()
+    S1.pivot_step()
+    print(S1.can_be_improved())
+    S1.get_pivot_position()
+    S1.pivot_step()
+    print(S1.can_be_improved())
+
+
+    print(S1.YS)
+
 
    # while S1.can_be_improved():
     #    S1.get_pivot_position()
